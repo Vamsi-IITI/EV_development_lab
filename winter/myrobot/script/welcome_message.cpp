34d8a3c3dfd972bad3b4f219522a3ed149205c6d
Vamsi-IITI <92263050+Vamsi-IITI@users.noreply.github.com>
2022-12-08 16:02:40 +0530

    Add files via upload

---
+++ winter/myrobot/script/welcome_message.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to the library of 2050s !\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
